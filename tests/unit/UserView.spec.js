d675b398ded3ee8a060a66648e740adeb76d35b9
matthew <homekeeper89@gmail.com>
2019-12-09 00:14:07 +0900

    [new] Fail 테스트 후 파일 생성

---
+++ tests/unit/UserView.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import UserView from '@/views/UserView'
+
+describe('UserView', () => {
+  it('renders the component', () => {
+    const wrapper = shallowMount(UserView)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
