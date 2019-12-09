2a618ea5c69ae61440b6d49b5b48634c3583185d
matthew <homekeeper89@gmail.com>
2019-12-10 01:36:23 +0900

    [new] 새로운 테스트 추가

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,10 +1,23 @@
 import { shallowMount } from '@vue/test-utils'
 import UserView from '@/views/UserView'
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
 
 describe('UserView', () => {
   it('renders the component', () => {
     const wrapper = shallowMount(UserView)
 
     expect(wrapper.html()).toMatchSnapshot()
+
+  })
+
+  it('renders main child component', () => {
+
+    const wrapper = shallowMount(UserView)
+    const userSearchForm = wrapper.find(VUserSearchForm)
+    const userProfile = wrapper.find(VUserProfile)
+
+    expect(userSearchForm.exists()).toBe(true)
+    expect(userProfile.exists()).toBe(true)
   })
 })
